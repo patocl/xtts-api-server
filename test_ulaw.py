d2c135413c7ca92f2e2ac94ca569106c011da1d6
Adrián Sosa <93357054+AdrianXira@users.noreply.github.com>
2024-09-27 18:45:27 -0600

    archivo para testear el endpoint uLaw

---
+++ test_ulaw.py
@@ -0,0 +1,40 @@
+import requests
+import base64
+import soundfile as sf
+import io
+
+# URL del endpoint
+url = "http://127.0.0.1:8002/tts_ulaw"
+
+# Parámetros para la solicitud
+params = {
+    'text': 'la vaca Lola tiene cabeza y tiene cola y hace Muuu!',
+    'speaker_wav': 'male.wav',
+    'language': 'es'
+}
+
+# Realizar la solicitud GET al endpoint
+response = requests.get(url, params=params, stream=True)
+
+# Verificar si la solicitud fue exitosa
+if response.status_code == 200:
+    # Crear un archivo para guardar la salida u-law
+    output_filename = "output_ulaw.wav"
+
+    # Preparar un buffer para almacenar los datos de audio u-law
+    ulaw_data = b""
+
+    # Leer la respuesta en streaming
+    for chunk in response.iter_lines():
+        if chunk:
+            # Decodificar cada bloque Base64 a bytes
+            ulaw_data += base64.b64decode(chunk)
+
+    # Escribir los datos u-law en un archivo
+    with io.BytesIO(ulaw_data) as ulaw_io:
+        data, samplerate = sf.read(ulaw_io, dtype='int16')
+        sf.write(output_filename, data, 8000, subtype='ULAW')
+
+    print(f"Audio saved as {output_filename}")
+else:
+    print(f"Failed to retrieve audio: {response.status_code}")
