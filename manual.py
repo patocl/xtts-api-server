97f6313e12c166569fb87722ca19bcef00146e55
Adrián Sosa <93357054+AdrianXira@users.noreply.github.com>
2024-09-27 20:19:36 -0600

    se cambia validacion de ficheros

--- manual.py
+++ manual.py
@@ -1,10 +1,22 @@
 import soundfile as sf
+import os
 
-filename = 'speakers/male_fixed.wav'
+# Ruta al archivo de audio con raw string
+file_path = r"C:\Codes\xtts-api-server\output\d8a53b84-f18d-4f5f-bb65-f3f38484a332.wav"
 
-# Intenta cargar el archivo para verificar que el encabezado es correcto
-try:
-    data, samplerate = sf.read(filename)
-    print(f"Archivo cargado correctamente. Muestreo: {samplerate}, Datos: {data.shape}")
-except Exception as e:
-    print(f"Error al cargar el archivo WAV: {e}")
+# Verificar si el archivo existe
+if os.path.exists(file_path):
+    try:
+        # Leer el archivo de audio
+        info = sf.info(file_path)
+        
+        # Mostrar la información del archivo de audio
+        print(f"Formato: {info.format}")
+        print(f"Canales: {info.channels}")
+        print(f"Frecuencia de muestreo: {info.samplerate} Hz")
+        print(f"Tipo de Subformato: {info.subtype}")
+    
+    except RuntimeError as e:
+        print(f"Error al leer el archivo de audio: {e}")
+else:
+    print(f"El archivo no existe: {file_path}")
